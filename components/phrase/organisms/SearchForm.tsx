3e5a7cf42223d37be79b9dad2a3949ad202a2920
cornchiplick <ilsunkiss@gmail.com>
2025-05-30 00:27:05 +0900

    Fix: SearchForm 의 FormInput 이 onKeyUp 함수를 받도록 조정

--- components/phrase/organisms/SearchForm.tsx
+++ components/phrase/organisms/SearchForm.tsx
@@ -1,7 +1,12 @@
 import FormInput from "@/components/common/FormInput";
 import Icon from "@/components/common/Icon";
+import {KeyboardEventHandler} from "react";
 
-const SearchForm = () => {
+interface SearchFormProps {
+  onKeyUp?: KeyboardEventHandler<HTMLInputElement>;
+}
+
+const SearchForm = ({onKeyUp}: SearchFormProps) => {
   return (
     <div className="relative w-full">
       <span className="absolute left-3 top-1/2 -translate-y-1/2">
@@ -12,6 +17,7 @@ const SearchForm = () => {
         name="keyword"
         placeholder="문장 검색..."
         className="w-full rounded-lg border p-2 pl-10"
+        onKeyUp={onKeyUp}
       />
     </div>
   );
