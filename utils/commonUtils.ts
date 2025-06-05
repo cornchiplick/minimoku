4cf8938e287319d801b24d8972827f77e9daa36f
cornchiplick <ilsunkiss@gmail.com>
2025-06-06 00:27:40 +0900

    Fix: getPathnameArray 함수 리팩토링
    - 루트 경로에서도 빈배열을 반환하지 않도록 수정
    - 해당 함수의 JsDoc을 작성함

--- utils/commonUtils.ts
+++ utils/commonUtils.ts
@@ -1,7 +1,15 @@
+/**
+ * 주어진 pathname 문자열을 '/'로 분리하여 각 경로 조각 앞에 '/'를 붙인 문자열 배열로 반환합니다.
+ *
+ * @param pathname usePathname()의 반환값
+ * @returns 각 경로 조각 앞에 '/'가 붙은 문자열 배열을 반환
+ * 예시: "/phrase/word" → ["/phrase", "/word"]
+ * 예시: "/" → ["/"]
+ */
 export const getPathnameArray = (pathname: string): string[] => {
   const pathArray = pathname
     .split("/")
-    .filter(Boolean)
-    .map((path) => `/${path}`);
+    .map((path) => `/${path}`)
+    .filter(Boolean);
   return pathArray;
 };
