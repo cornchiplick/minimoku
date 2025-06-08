8e5237cae1a2e8bf9cb83723fca68dabdf14cf64
cornchiplick <ilsunkiss@gmail.com>
2025-06-08 19:50:53 +0900

    Fix: ReadCell 컴포넌트의 title 부분의 너비를 조절할 수 있도록 스타일 및 prop 수정

--- components/common/ReadCell.tsx
+++ components/common/ReadCell.tsx
@@ -3,14 +3,15 @@ import {cn} from "@/lib/utils";
 
 interface ReadCellProps {
   title: string;
+  titleWidth?: string;
   className?: string;
   children: React.ReactNode;
 }
 
-const ReadCell = ({title, className, children}: ReadCellProps) => {
+const ReadCell = ({title, titleWidth = "w-40", className, children}: ReadCellProps) => {
   return (
     <div className="flex min-h-10 self-stretch">
-      <div className="flex min-h-10 w-40 items-center">
+      <div className={cn("flex min-h-10 items-center", titleWidth)}>
         <Typography.P1 className={cn("font-bold", className)}>{title}</Typography.P1>
       </div>
       <div className="flex min-h-10 flex-1 flex-wrap content-center items-center">{children}</div>
