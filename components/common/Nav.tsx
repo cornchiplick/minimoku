44ae6c145077d6fec4e559e836526f83f606c650
cornchiplick <ilsunkiss@gmail.com>
2025-07-22 14:56:41 +0900

    Fix: 현재 로그인 상태인지 체크하여 화면 렌더링 수정

--- components/common/Nav.tsx
+++ components/common/Nav.tsx
@@ -2,14 +2,14 @@
 
 import Icon from "@/components/common/Icon";
 import {URL} from "@/constants/url";
+import {useAuth} from "@/hooks/useAuth";
+import {signOut} from "next-auth/react";
 import Link from "next/link";
 import {usePathname} from "next/navigation";
 
 const Nav = () => {
   const pathname = usePathname();
-
-  // TODO 로그인 상태를 관리하는 상태 변수 : next/auth 로 구현할것
-  const isLoggedIn = true;
+  const {isAuthenticated} = useAuth();
 
   return (
     <nav className="w-full bg-indigo-600 px-6 py-2 text-white">
@@ -25,13 +25,13 @@ const Nav = () => {
             <Icon name="home" size={16} color="white" />홈
           </Link>
           <Link
-            href={isLoggedIn ? URL.PHRASES : URL.LOGIN}
+            href={URL.PHRASES}
             className={`flex items-center gap-1 rounded px-3 py-2 hover:bg-indigo-700 ${pathname === URL.PHRASES ? "bg-indigo-700" : ""}`}>
             <Icon name="listcheck" size={16} color="white" />
             문장목록
           </Link>
           <Link
-            href={isLoggedIn ? URL.QUIZ : URL.LOGIN}
+            href={URL.QUIZ}
             className={`flex items-center gap-1 rounded px-3 py-2 hover:bg-indigo-700 ${pathname === "quiz-ja-ko" ? "bg-indigo-700" : ""}`}>
             <Icon name="barchartver" size={16} color="white" />
             퀴즈
@@ -42,11 +42,9 @@ const Nav = () => {
             <Icon name="table" size={16} color="white" />
             문자표
           </Link>
-          {/* 
-            // TODO Link 태그를 사용해서 다른 URL 로 이동시킬 것 (로그인인 경우 로그인 모달을 사용할 것인지 페이지를 넘길 것인지 먼저 결정) */}
-          {isLoggedIn ? (
+          {isAuthenticated ? (
             <button
-              // onClick={handleLogout}
+              onClick={() => signOut({callbackUrl: URL.HOME})}
               className="flex items-center gap-1 rounded px-3 py-2 hover:bg-indigo-700">
               <Icon name="logout" size={16} color="white" />
               로그아웃
--- components/common/SubHeader.tsx
+++ components/common/SubHeader.tsx
@@ -3,19 +3,21 @@
 import Icon from "@/components/common/Icon";
 import Typography from "@/components/home/atomic/Typography";
 import {URL, URL_TITLE_MAP} from "@/constants/url";
+import {useAuth} from "@/hooks/useAuth";
 import {getPathnameArray} from "@/utils/commonUtils";
 import {usePathname} from "next/navigation";
 
 const SubHeader = () => {
-  // TODO isLoggedIn
-  const isLoggedIn = true;
   const pathname = usePathname();
   const firstPath = getPathnameArray(pathname).at(0) || URL.HOME;
+  const {isAuthenticated} = useAuth();
+  // const {status} = useSession();
+  // const isAuthenticated = useMemo(() => status === AuthConstants.AUTHENTICATED, [status]);
 
   return (
     <Typography.Head3 className="flex items-center gap-2">
       <Icon name={URL_TITLE_MAP[firstPath].icon} size={24} color="black" />
-      {isLoggedIn ? URL_TITLE_MAP[firstPath].login : URL_TITLE_MAP[firstPath].logout}
+      {isAuthenticated ? URL_TITLE_MAP[firstPath].login : URL_TITLE_MAP[firstPath].logout}
     </Typography.Head3>
   );
 };
