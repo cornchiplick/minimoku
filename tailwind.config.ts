c7086813765fcb1d02e2baa36c9f6c9e5340cc0b
cornchiplick <ilsunkiss@gmail.com>
2025-09-22 00:08:38 +0900

    Fix: 다크모드 설정을 수정함

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,6 @@
 module.exports = {
   theme: {
-    extend: {
-      colors: {
-        background: "var(--color-background)", // 기본 배경색
-        foreground: "var(--color-foreground)", // 글자, 아이콘 등 전경 요소의 기본 색상
-      },
-      fontFamily: {
-        sans: "var(--font-sans)",
-        mono: "var(--font-mono)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
