0201529a84a6224911a9acbe2b78e5dc23aa1b10
cornchiplick <ilsunkiss@gmail.com>
2025-11-18 15:35:46 +0900

    Feat: 폴더 액션을 위한 드롭다운 구조 만들기

--- src/features/navigation/ui/FolderItem.tsx
+++ src/features/navigation/ui/FolderItem.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import {FolderInterface} from "@/entities/folder/types";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/shared/components/atoms/dropdown";
 import Icon from "@/shared/components/molecules/Icon";
 import Typography from "@/shared/home/atomic/Typography";
 import {useThemeStore} from "@/shared/store/themeStore";
@@ -15,6 +21,11 @@ interface FolderItemProps {
 const FolderItem = ({folder, onClick}: FolderItemProps) => {
   const {isDarkMode} = useThemeStore();
   const FolderIcon = folder.id === 0 ? Archive : Folder;
+
+  const handleClickMore = (e: any) => {
+    e.stopPropagation();
+    console.log("test");
+  };
 
   return (
     <div
@@ -32,9 +43,27 @@ const FolderItem = ({folder, onClick}: FolderItemProps) => {
         <Typography.P2>{folder.name}</Typography.P2>
         <Typography.P3>{folder.count}</Typography.P3>
       </div>
-      <button type="button">
-        <Icon name="more" color="#99a1af" size={16} />
-      </button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <button type="button" className="cursor-pointer rounded-full p-1 hover:bg-gray-300">
+            <Icon name="more" color="#99a1af" size={16} />
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          side="right"
+          align="start"
+          className="bg-background-primary w-28 border border-gray-400">
+          <DropdownMenuItem>
+            <Typography.P2>수정</Typography.P2>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Typography.P2>삭제</Typography.P2>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Typography.P2>비우기</Typography.P2>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
