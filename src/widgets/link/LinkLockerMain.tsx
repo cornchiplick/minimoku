b44415826142a2cc6a583e495645989556391a2b
cornchiplick <ilsunkiss@gmail.com>
2025-12-11 20:56:26 +0900

    Fix: 링크 검색결과 목록에서 검색어에 하이라이트 처리

--- src/widgets/link/LinkLockerMain.tsx
+++ src/widgets/link/LinkLockerMain.tsx
@@ -6,17 +6,23 @@ import Divider from "@/shared/components/molecules/Divider";
 import Icon from "@/shared/components/molecules/Icon";
 import Typography from "@/shared/home/atomic/Typography";
 import ThemeToggleButton from "@/widgets/sidebar/ThemeToggleButton";
+import {useSearchParams} from "next/navigation";
 
 interface LinkLockerMainProps {
   initialLinks: LinkInterface[];
 }
 
 export default function LinkLockerMain({initialLinks}: LinkLockerMainProps) {
+  const searchParams = useSearchParams();
+  const keyword = searchParams.get("keyword");
+
   return (
     <div className="flex flex-1 flex-col">
       {/* Content Header */}
       <div className="bg-background-primary flex flex-row items-center justify-between px-6 py-4">
-        <Typography.SubTitle1 className="font-semibold">최근 링크</Typography.SubTitle1>
+        <Typography.SubTitle1 className="font-semibold" keyword={keyword}>
+          {!!keyword ? `검색: ${keyword}` : "최근 링크"}
+        </Typography.SubTitle1>
         <ThemeToggleButton />
       </div>
 
@@ -26,7 +32,9 @@ export default function LinkLockerMain({initialLinks}: LinkLockerMainProps) {
       <div className="bg-background-secondary flex-1 overflow-y-auto p-6">
         <div className="w-full space-y-4">
           {!!initialLinks.length ? (
-            initialLinks.map((linkData) => <LinkCard key={linkData.id} data={linkData} />)
+            initialLinks.map((linkData) => (
+              <LinkCard key={linkData.id} data={linkData} keyword={keyword} />
+            ))
           ) : (
             <div className="flex min-h-[400px] flex-col items-center justify-center space-y-4">
               <Icon name="box" size={120} color="white" />
